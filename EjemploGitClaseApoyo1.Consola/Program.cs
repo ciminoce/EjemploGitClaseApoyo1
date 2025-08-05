6420d38564cfd7ec2e3e7db74d02ddcaba74c0f8
ciminoce <ciminoce@yahoo.com.ar>
2025-08-05 19:21:00 -0300

    Añadir funcionalidad para informar el día de la semana
    Se ha implementado un nuevo método `InformarDiaSemana` que devuelve el nombre del día en español según el número de día ingresado. Este método se utiliza en el bucle principal del programa para mostrar el día correspondiente antes de solicitar la temperatura al usuario.

--- EjemploGitClaseApoyo1.Consola/Program.cs
+++ EjemploGitClaseApoyo1.Consola/Program.cs
@@ -7,9 +7,29 @@
             for (int contadorTemperaturas = 1; contadorTemperaturas <= 5; contadorTemperaturas++)
             {
                 Console.WriteLine($"Nro. Temperatura:{contadorTemperaturas}");
+                Console.WriteLine($"Nombre del dia: {InformarDiaSemana(contadorTemperaturas)}");
                 Console.Write("Ingrese una temperatura entre -10 y 30:");
                 var temperaturaCelsius = double.Parse(Console.ReadLine()!);
 
+            }
+        }
+        private static string InformarDiaSemana(int nroDia)
+        {
+            switch (nroDia)
+            {
+                case 1:
+                    return "Lunes";
+                case 2:
+                    return "Martes";
+                case 3:
+                    return "Miércoles";
+                case 4:
+                    return "Jueves";
+                case 5:
+                    return "Viernes";
+                default:
+
+                    return "Día no válido";
             }
         }
     }
