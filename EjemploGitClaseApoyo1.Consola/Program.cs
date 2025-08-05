dc59f4c621c7409f23f1a6be86bbd45842f07eb2
ciminoce <ciminoce@yahoo.com.ar>
2025-08-05 19:13:45 -0300

    Agregar archivos de proyecto.

---
+++ EjemploGitClaseApoyo1.Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EjemploGitClaseApoyo1.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contadorTemperaturas = 1; contadorTemperaturas <= 5; contadorTemperaturas++)
+            {
+                Console.WriteLine($"Nro. Temperatura:{contadorTemperaturas}");
+                Console.Write("Ingrese una temperatura entre -10 y 30:");
+                var temperaturaCelsius = double.Parse(Console.ReadLine()!);
+
+            }
+        }
+    }
+}
